1de7111a3cd1c65ade0954617883a8fc2c783ea1
Samuel Rendl <samuelrichardrendl@gmail.com>
2025-07-13 12:24:55 +0200

    add styling to OAuth buttons

--- components/auth/SignInButton.tsx
+++ components/auth/SignInButton.tsx
@@ -1,15 +1,27 @@
 import { signIn } from "@/auth";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const buttonStyle = "w-full";
 
 export function SignIn() {
   return (
-    <div>
+    <div className="flex gap-1">
       <form
         action={async () => {
           "use server";
           await signIn("github", { redirectTo: "/dashboard" });
         }}
       >
-        <button type="submit">Sign in with Github</button>
+        <Button type="submit" variant={"outline"} className={buttonStyle}>
+          <Image
+            src={"/github/github-mark.svg"}
+            alt="Github Icon"
+            height={20}
+            width={20}
+          />
+          Continue with Github
+        </Button>
       </form>
       <form
         action={async () => {
@@ -17,7 +29,15 @@ export function SignIn() {
           await signIn("google", { redirectTo: "/dashboard" });
         }}
       >
-        <button type="submit">Sign in with Google</button>
+        <Button type="submit" variant={"outline"} className={buttonStyle}>
+          <Image
+            src={"/google/google-icon.svg"}
+            alt="Google Icon"
+            height={20}
+            width={20}
+          />
+          Continue with Google
+        </Button>
       </form>
     </div>
   );
